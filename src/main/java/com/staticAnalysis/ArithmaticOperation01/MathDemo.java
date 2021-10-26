02e445a99b6fed753795e8e4cebe0f24534561de
Pranali14932 <92152803+Pranali14932@users.noreply.github.com>
2021-10-26 01:36:03 -0700

    Update MathDemo.java

--- src/main/java/com/staticAnalysis/ArithmaticOperation01/MathDemo.java
+++ src/main/java/com/staticAnalysis/ArithmaticOperation01/MathDemo.java
@@ -1,9 +1,5 @@
 package com.staticAnalysis.ArithmaticOperation01;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
 public class MathDemo {
 	public int add(int a,int b){
 		return a+b;
